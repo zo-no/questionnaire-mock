095b37029129feb0992fc13670b9080b213ba455
zo-no <2742160682@qq.com>
2024-03-06 01:35:19 +0800

    mock完成B端开发

--- mock/index.js
+++ mock/index.js
@@ -1,9 +1,9 @@
 // const test = require("./test");
 const question = require("./question");
 const user = require("./user");
-// const stat = require("./stat");
+const stat = require("./stat.js");
 // const answer = require("./answer");
 
-const mockList = [...question, ...user];
+const mockList = [...question, ...user, ...stat];
 
 module.exports = mockList;
